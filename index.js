86f11df073b7cede52283d1edfc096f10d29e195
Nadezda.chikurova <chikurova90@gmail.com>
2023-01-11 11:41:01 -0700

    added d3

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let state = {
   selectedMetric: null,
 };
 
-d3.csv("../../data/statePopulations.csv", d3.autoType).then((data) => {
+d3.csv("../data/statePopulations.csv", d3.autoType).then((data) => {
   console.log("data", data);
   state.data = data;
   state.domain = [
